bc1b69355844fd7bfc0e9c72d0081e12610bf7ee
Ezra Szanton <ezraszanton@yahoo.com>
2020-05-22 21:29:12 -0400

    added sin scene

---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0;
+        transform.position = mouse;
+    }
+}
---
+++ Assets/wave.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wave : MonoBehaviour
+{
+    LineRenderer line;
+
+    Vector3[] points;
+
+    Vector3 start;
+
+    public float grain;
+    public float length;
+
+    public GameObject mouse;
+
+    int numPoints;
+
+    public int mult;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        start = Vector3.Lerp(Vector3.zero, mouse.transform.position, 0.2f);
+
+        length = 10 * Mathf.Abs(mouse.transform.position.x);
+
+        numPoints = (int)(length / grain);
+        points = new Vector3[numPoints];
+        
+        for(int i = 0; i < numPoints; i++)
+        {
+            float x = i * grain * mult * Mathf.Sign(mouse.transform.position.x);
+            float y = Mathf.Sin((6 * x) / Mathf.Abs(mouse.transform.position.x)) * mouse.transform.position.y;
+            points[i] = new Vector3(x + start.x, y + start.y, 0);
+        }
+
+        line.positionCount = numPoints;
+        line.SetPositions(points);
+    }
+}
