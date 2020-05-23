67811b91d6b394d6b68eefd465fd9ffe7d050638
Ezra Szanton <ezraszanton@yahoo.com>
2020-05-23 10:42:38 -0400

    baseball throws

---
+++ Assets/pitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pitch : MonoBehaviour
+{
+    public float waitTime;
+    public float pitchSpeed;
+
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        StartCoroutine( waitToThrow() );
+    }
+
+    IEnumerator waitToThrow()
+    {
+        yield return new WaitForSeconds(waitTime);
+        body.velocity = Vector3.left * pitchSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("winzone"))
+        {
+            body.velocity = Vector3.zero;
+            Debug.Log("you win!");
+        }
+    }
+}
