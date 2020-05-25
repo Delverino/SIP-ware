fd4cad5b95fa8fc114b02e5dde413805d58fcb6b
Joseph Henry Stadolnik IV <hstadolnik@gmail.com>
2020-05-25 17:59:22 -0400

    Added ending clouds

---
+++ Assets/Stargaze/Scripts/Cloud.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    private Vector3 endPos;
+    private bool moving = false;
+    public float minOffset;
+    public float maxOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start with random model
+        string cloudType = "voxcloud"+Random.Range(1,5);
+        Mesh cloudMesh = Resources.Load<Mesh>("Voxels/"+cloudType);
+        Material cloudMat = Resources.Load<Material>("Voxels/materials/mat_"+cloudType);
+        MeshFilter mf = GetComponent<MeshFilter>();
+        mf.mesh = cloudMesh;
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        mr.material = cloudMat;
+
+        // start with random offset
+        endPos = transform.position;
+        transform.Translate(Random.Range(minOffset,maxOffset)*(Random.Range(0,2)*2-1), 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // lerp into place on command
+        if (moving)
+        {
+            transform.Translate((endPos.x-transform.position.x)/8, 0, 0);
+        }
+    }
+
+    // Call this via broadcast to make clouds move into place
+    public void StartMoving()
+    {
+        moving = true;
+    }
+}
--- Assets/Stargaze/Scripts/Constellation.cs
+++ Assets/Stargaze/Scripts/Constellation.cs
@@ -11,10 +11,23 @@ public class Constellation : MonoBehaviour
     public Bounds starBounds;
     public uint numberOfStars;
     public GameObject starPrefab;
+    public GameObject cloudPrefab;
 
     void Awake()
     {
         Debug.Log(starBounds.extents);
+        
+        int numberOfClouds = Random.Range(12,15);
+        for (int ii = 0; ii < numberOfClouds; ii++)
+        {
+            Vector3 randomPoint = new Vector3(
+                Random.Range(starBounds.min.x, starBounds.max.x),
+                Random.Range(starBounds.min.y, starBounds.max.y),
+                -10.0f);
+
+            GameObject instance = Instantiate(cloudPrefab, transform);
+            instance.transform.localPosition = randomPoint;
+        }
 
         for (int ii = 0; ii < numberOfStars; ii++)
         {
